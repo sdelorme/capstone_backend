9afbd8d6210a22ad918af1c8bb5ed8308fa6620d
Seth Delorme <delorme.s.m@gmail.com>
2021-04-08 21:21:34 -0500

    adds to seeds file with cdc data through 1 year, has not seeded db yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,187 @@
 #milestone test
 # milestone = Milestone.new(description: "Begins to smile at people", stage_id: 1)
 # milestone.save
-# milestone = Milestone.new(description: "Can briefly calm themselves (may bring hands to mouth and suck on hand", stage_id: 1)
-# milestone.save
-# milestone = Milestone.new(description: "Tries to look at parent", stage_id: 1)
-# milestone.save
+milestone = Milestone.new(description: "Can briefly calm themselves (may bring hands to mouth and suck on hand", stage_id: 1, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Tries to look at parent", stage_id: 1, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Coos, makes gurgling sounds", stage_id: 1, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Turns head toward sounds", stage_id: 1, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Pays attention to faces", stage_id: 1, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Begins to follow things with eyes and recognize people at a distance", stage_id: 1, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Begins to act bored (cries, fussy) if activity doesn't change", stage_id: 1, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Can hold head up and begins to push up when lying on tummy", stage_id: 1, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Makes smoother movements with arms and legs", stage_id: 1, milestone_categories_id: 4)
+milestone.save
+
+milestone = Milestone.new(description: "Smiles spontaneously, especially at people", stage_id: 2, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Likes to play with people and might cry when playing stops", stage_id: 2, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Copies some movements and facial expressions, like smiling or frowning", stage_id: 2, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Begins to babble", stage_id: 2, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Babbles with expression and copies sounds they hear", stage_id: 2, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Cries in different ways to show hunger, pain, or being tired", stage_id: 2, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Lets you know if they are happy or sad", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Responds to affection", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Reaches for toy with one hand", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Uses hands and eyes together, such as seeing a toy and reaching for it", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Follows moving things with eyes from side to side", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Watches faces closely", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Recognizes familiar people and thing at a distance", stage_id: 2, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Holds head steady, unsupported", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Pushes down on legs when feet are on a hard surface", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "May be able to roll over from tummy to back", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Can hold a toy and shake it and swing at dangling toys", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Brings hands to mouth", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "When lying on stomach, pushes up to elbows", stage_id: 2, milestone_categories_id: 4)
+milestone.save
+
+milestone = Milestone.new(description: "Knows familiar faces and begins to know if someone is a stranger", stage_id: 3, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Likes to play with others, especially parents", stage_id: 3, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Responds to other people's emotions and often seems happy", stage_id: 3, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Likes to look at self in a mirror", stage_id: 3, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Responds to sounds by making sounds", stage_id: 3, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Strings vowels together when babbling --'ah', 'eh', 'oh' -- and likes taking turns with parent while making sounds", stage_id: 3, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Responds to own name", stage_id: 3, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Makes sounds to show joy and displeasure", stage_id: 3, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Begins to say consonant sounds(jabbering with 'm', 'b')", stage_id: 3, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Looks around at things nearby", stage_id: 3, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Brings things to mouth", stage_id: 3, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Shows curiousity about things and tries to get things that are out of reach", stage_id: 3, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Begins to pass things from one hand to the other", stage_id: 3, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Rolls over in both directions(front to back, back to front)", stage_id: 3, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Begins to sit without support", stage_id: 3, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "When standing, suports weight on legs and might bounce", stage_id: 3, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Rocks back and forth, sometimes crawling backward before moving forward", stage_id: 3, milestone_categories_id: 4)
+milestone.save
+
+milestone = Milestone.new(description: "May be afraid of strangers", stage_id: 4, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "May be clingy eith familiar adults", stage_id: 4, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Has favorite toys", stage_id: 4, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Understands 'no'", stage_id: 4, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Makes a lot of different sounds like 'mamamamama' and 'bababababa'", stage_id: 4, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Copies sounds and gestures of others", stage_id: 4, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Uses fingers to point at things", stage_id: 4, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Watches the path of something as it falls", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Looks for things that they see you hide", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Play peek-a-boo", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Puts things in their mouth", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Moves things smoothly from one hand to the other", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Picks up things like cereal o's between thumb and index finger", stage_id: 4, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Stands, holding on", stage_id: 4, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Can get into sitting position", stage_id: 4, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Sits without support", stage_id: 4, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Pulls to stand", stage_id: 4, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Crawls", stage_id: 4, milestone_categories_id: 4)
+milestone.save
+
+milestone = Milestone.new(description: "Is shy or nervous with strangers", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Cries when mom or dad leaves", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Has favorite things and people", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Shows fear in some situations", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Hands you a book when they want to hear a story", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Repeats sounds or actions to get attention", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Puts out arm or leg to help with dressing", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Plays games such as 'peek-a-boo' and 'pat-a-cake'", stage_id: 5, milestone_categories_id: 1)
+milestone.save
+milestone = Milestone.new(description: "Responds to simple spoken requests", stage_id: 5, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Uses simple gestures, like shaking head 'no' or waving 'bye-bye'", stage_id: 5, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Makes sounds with changes in tone (sounds more like speech)", stage_id: 5, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Says 'mama' and 'dada' and  exclamations like 'uh-oh!'", stage_id: 5, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Tries to say words you say", stage_id: 5, milestone_categories_id: 2)
+milestone.save
+milestone = Milestone.new(description: "Explores things in different ways, like shaking, banging, throwing", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Finds hidden things easily", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Looks at the right picture or thing when it's named", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Copies gestures", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Starts to use things correctly; for example, drinks from a cup, brushes hair", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Bangs two things together", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Puts things in a container, takes things out of a container", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Lets things go without help", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Pokes with index finger", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Follows simple directions like 'pick up the toy'", stage_id: 5, milestone_categories_id: 3)
+milestone.save
+milestone = Milestone.new(description: "Gets to a sitting position without help", stage_id: 5, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "Pulls up to stand, walks holding on to furniture('cruising')", stage_id: 5, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "May take a few steps without holding on", stage_id: 5, milestone_categories_id: 4)
+milestone.save
+milestone = Milestone.new(description: "May stand alone", stage_id: 5, milestone_categories_id: 4)
+milestone.save
