4ba27120185c486401a7e4a548a714b6d0d864b1
Seth Delorme <delorme.s.m@gmail.com>
2021-04-15 21:16:08 -0500

    adds association between milestone and milestone_categories

--- db/migrate/20210416021215_change_milestone_categories_id_to_milestone_category_id.rb
+++ db/migrate/20210416021215_change_milestone_categories_id_to_milestone_category_id.rb
@@ -1,5 +1,5 @@
 class ChangeMilestoneCategoriesIdToMilestoneCategoryId < ActiveRecord::Migration[6.1]
   def change
-    rename_column :milestones, :milestone_categories_id, :milestone_category_id
+    rename_column :milestones, :milestone_categories_id, :milestone_category_id 
   end
 end
