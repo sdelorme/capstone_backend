aacb241c611a86b7cea0641dcbf0a27cdc7102c4
Seth Delorme <delorme.s.m@gmail.com>
2021-04-22 20:24:29 -0500

    adds simple show and index for children

--- app/controllers/api/children_controller.rb
+++ app/controllers/api/children_controller.rb
@@ -1,2 +1,11 @@
 class Api::ChildrenController < ApplicationController
+  def index
+    @children = Child.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @child = Child.find(params[:id])
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    #milestones
     get "/milestones" => "milestones#index"
     get "/milestones/:id" => "milestones#show"
+    #children
+    get "/children" => "children#index"
+    get "/children/:id" => "children#show"
   end
 end
